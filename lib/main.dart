5cd6dcb62aace9fbf4a51e8c4c43b312c7c4ea3e
Manculix <f.arief.p@gmail.com>
2022-11-08 20:38:49 +0700

    test_terbaru

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Intensif Club'),
+          title: const Text('Flutter Intensif Club'),
         ),
-        body: Center(
+        body: const Center(
           child: Text('Hello Gaes'),
         ),
       ),
